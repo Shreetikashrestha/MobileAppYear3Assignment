b7fe3a8407cce89c3cbfb3724497e844b6985e8a
ShreetikaShrestha <shreetika59@gmail.com>
2025-12-05 09:43:42 +0545

    1st sprint done

---
+++ android/app/src/main/kotlin/com/shreetika/batch35a_flutter/influcollb_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shreetika.batch35a_flutter.influcollb_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+import 'screens/onboardingscreens/onboarding1_screen.dart';
+import 'screens/onboardingscreens/onboarding2_screen.dart';
+import 'screens/onboardingscreens/onboarding3_screen.dart';
+// import 'screens/onboarding/onboarding_screen3.dart'; // Add when you create it
+import 'screens/loginSignup/signup_screen.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Influcollab App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        useMaterial3: true,
+      ),
+      // Set splash screen as the initial route
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/onboarding1': (context) => const OnboardingScreen1(),
+        '/onboarding2': (context) => const OnboardingScreen2(),
+        '/onboarding3': (context) => const OnboardingScreen3(),
+        '/signup': (context) => const SignupScreen(),
+        // '/login': (context) => const LoginScreen(),
+        // '/register': (context) => const RegisterScreen(),
+        // '/home': (context) => const HomeScreen(),
+      },
+    );
+  }
+}
