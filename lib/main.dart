e87fc879d2d77df1070324140d422f5f088af1cd
ShreetikaShrestha <shreetika59@gmail.com>
2025-12-19 13:00:39 +0545

    updated pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         useMaterial3: true,
       ),
-      // Set splash screen as the initial route
+
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
