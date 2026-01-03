03e60af7eff2c7954f4237d7df2150f95e7e855b
ShreetikaShrestha <shreetika59@gmail.com>
2026-01-03 17:19:30 +0545

    features home 1

---
+++ lib/features/home/presentation/pages/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, '/onboarding1');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset("assets/images/splashbg.jpg", fit: BoxFit.cover),
+          Center(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Image.asset(
+                "assets/images/logo.png",
+                width: 120,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
