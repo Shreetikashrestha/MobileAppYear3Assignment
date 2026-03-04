e9ab67bcfa195fc4da6b556f064e877597e1770f
ShreetikaShrestha <shreetika59@gmail.com>
2026-03-04 23:32:08 +0545

    logout usecase added

--- lib/features/auth/domain/usecases/logout_usecase.dart
+++ lib/features/auth/domain/usecases/logout_usecase.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:influcollb_app/core/error/failures.dart';
+import 'package:influcollb_app/core/error/failure.dart';
 import 'package:influcollb_app/core/usecases/usecase.dart';
 import 'package:influcollb_app/features/auth/domain/repositories/auth_repository.dart';
 
