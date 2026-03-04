43551122119c0d1b378b8b9351b7c4b2d9b258a2
ShreetikaShrestha <shreetika59@gmail.com>
2026-03-04 23:31:40 +0545

    application card widget added

--- lib/features/application/presentation/widgets/application_card.dart
+++ lib/features/application/presentation/widgets/application_card.dart
@@ -90,7 +90,7 @@ class ApplicationCard extends StatelessWidget {
                     padding:
                         const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
                     decoration: BoxDecoration(
-                      color: statusColor.withOpacity(0.1),
+                      color: statusColor.withValues(alpha: 0.1),
                       borderRadius: BorderRadius.circular(20),
                       border: Border.all(color: statusColor, width: 1),
                     ),
