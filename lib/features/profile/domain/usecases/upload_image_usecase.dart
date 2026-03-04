45ec7ca77a9e9d7aac06ff10f2fefac366c4e946
ShreetikaShrestha <shreetika59@gmail.com>
2026-03-04 23:35:04 +0545

    upload image usecase added

--- lib/features/profile/domain/usecases/upload_image_usecase.dart
+++ lib/features/profile/domain/usecases/upload_image_usecase.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:dartz/dartz.dart';
-import 'package:influcollb_app/core/error/failures.dart';
+import 'package:influcollb_app/core/error/failure.dart';
 import 'package:influcollb_app/core/usecases/usecase.dart';
 import '../repositories/profile_repository.dart';
 
